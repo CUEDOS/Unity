4ed347c907b43142136042ee5addb413efe47873
Samuel Knox <47692324+SamJCKnox@users.noreply.github.com>
2020-06-15 15:33:13 +0100

    Good

---
+++ Assets/Sam/SimpleCityGenerator/Editor/BlockCityGeneratorEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SimpleCityGenerator))]
+public class BlockCityGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        SimpleCityGenerator spawner = (SimpleCityGenerator)target;
+
+        if (GUILayout.Button("Spawn Objects"))
+        {
+            spawner.EditorSpawn();
+
+        }
+
+        if (GUILayout.Button("Remove Spawns"))
+        {
+            spawner.DestroyAll();
+        }
+    }
+}
+
